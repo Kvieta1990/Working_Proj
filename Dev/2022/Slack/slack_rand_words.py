a4e7381109a16590f834514433b710ea7349c520
Yuanpeng Zhang <zyroc1990@gmail.com>
2022-12-29 14:35:37 -0500

    add in slack msg app

--- Dev/2022/Slack/slack_rand_words.py
+++ Dev/2022/Slack/slack_rand_words.py
@@ -1,5 +1,5 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-client = WebClient(token="xoxb-27997197269-4579628452532-KfsiDmfcu70kAv9HU12zo2jb")
-client.chat_postMessage(channel="words", text="Hello world!")
+client = WebClient(token="xoxb-27997197269-4589997032001-J8TqVj7wGnvRDpG7DgMiq6eb")
+client.chat_postMessage(channel="memorize", text="Hello world!")
